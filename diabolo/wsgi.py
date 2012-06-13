ff07d553f0b40ed96edf75e1160a78bf1e8b794c
Arthur Puyou <arthur@puyou.fr>
2012-06-13 02:49:22 +0200

    maj wsgi

--- diabolo/wsgi.py
+++ diabolo/wsgi.py
@@ -13,9 +13,10 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "payutc.settings")
+sys.path.append('/usr/share/diabolo')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diabolo.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
